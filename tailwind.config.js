6b163906f157f45646f4c713d51a81743de64930
Prajwal Nagpure <91003772+prajwalnagpure077@users.noreply.github.com>
2024-12-29 05:22:55 +0530

    base layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["public/*.{html,js}", "./index.html"],
+  content: ["public/*.{html,js}", "./index.html", "./EasyAim.html"],
   theme: {
     extend: {},
   },
