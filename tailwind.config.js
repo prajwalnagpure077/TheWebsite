2e10d3f6d1bbaadfe80a97055d9f310da2fcb99c
Prajwal Nagpure <91003772+prajwalnagpure077@users.noreply.github.com>
2025-01-10 10:19:53 +0530

    privacy policy + terms of service

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["public/*.{html,js}", "./index.html", "./EasyAim.html","./privacy-policy.html"],
+  content: ["public/*.{html,js}", "./index.html", "./EasyAim.html","EasyAim/*.{html,js}"],
   theme: {
     extend: {},
   },
