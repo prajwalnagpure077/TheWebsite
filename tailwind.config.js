0a837cee7f361b2fb543801906098f398544b68f
Prajwal Nagpure <91003772+prajwalnagpure077@users.noreply.github.com>
2025-01-14 20:58:57 +0530

    few animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["public/*.{html,js}", "./index.html", "./EasyAim.html","EasyAim/*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      animation: {
+        wobble: 'wobble 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wobble: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+    },
+},
   plugins: [],
 }
-
